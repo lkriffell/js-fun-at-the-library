29a2b1e480c02f27113f3ed4f5edbfdfee9b3d21
Logan Riffell <lkriffell@gmail.com>
2021-02-02 11:48:31 -0700

    Add code to make book tests pass

--- src/book.js
+++ src/book.js
@@ -1,12 +1,43 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return 'The ' + title
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+          name: name,
+          age: age,
+          pronouns: pronouns
+        }
+}
+
+function saveReview(new_review, reviews) {
+  if (!reviews.includes(new_review)) {
+    reviews.push(new_review)
+  }
+}
+
+function calculatePageCount(title) {
+  return title.length * 20
+}
+
+function writeBook(title, character, genre) {
+  return {
+          title: title,
+          mainCharacter: character,
+          pageCount: calculatePageCount(title),
+          genre: genre
+        }
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
