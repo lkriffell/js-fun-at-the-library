689a1795e30ed8c09f1e475a8d82e6a031397d8a
Logan Riffell <lkriffell@gmail.com>
2021-02-02 13:20:11 -0700

    Add code to make shelf tests pass

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,38 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+}
+
+function unshelfBook(bookName, shelf) {
+  shelf.forEach(function (book, index) {
+    if (book.title == bookName) {
+      shelf.splice(index, 1)
+    }
+  });
+}
+
+function listTitles(shelf) {
+  var list = ''
+  shelf.forEach(function (book) {
+    list += `${book.title}, `
+  });
+  return list.slice(0,-1).slice(0,-1)
+}
+
+function searchShelf(shelf, bookName) {
+  var included = false
+  shelf.forEach(function (book, index) {
+    if (book.title == bookName) {
+      included = true
+    }
+  });
+  return included
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
